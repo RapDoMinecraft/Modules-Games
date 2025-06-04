1748f5197611106dd392152aab62936469fe68eb
Venom-devX <davilfluna@gmail.com>
2025-06-04 12:19:36 -0300

    dfegtrvrfdsfgrtvegrdsfgrtht

--- Universal/VersionsHash.lua
+++ Universal/VersionsHash.lua
@@ -2,7 +2,7 @@ return {
     ["Gym League"] = "version-0c1fb77cdc3374b338b5c9d5596e59a5efde11fd0c3a81968216286f60d3f8f7";
     ["Brookhaven RP"] = "version-0c1fb77cdc3374b338b5c9d5596e59a5efde11fd0c3a81968216286f60d3f8f7";
     ["Blox Fruits"] = "version-0c1fb77cdc3374b338b5c9d5596e59a5efde11fd0c3a81968216286f60d3f8f7";
-    ["Blue Lock: Rivals"] = "version-0c1fb77cdc3374b338b5c9d5596e59a5efde11fd0c3a81968216286f60d3f8f7";
+    ["Blue Lock: Rivals"] = "version-9fb058abac731f5a17aac60dbdfbad215a875fb0b299751573389ae71fcbf13a";
     ["Murder Mystery 2"] = "version-0c1fb77cdc3374b338b5c9d5596e59a5efde11fd0c3a81968216286f60d3f8f7";
     ["Fighting Academy"] = "version-0c1fb77cdc3374b338b5c9d5596e59a5efde11fd0c3a81968216286f60d3f8f7";
     ["Dead Rails"] = "version-0c1fb77cdc3374b338b5c9d5596e59a5efde11fd0c3a81968216286f60d3f8f7";
